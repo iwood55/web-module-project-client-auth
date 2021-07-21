1f55f5a3eeddde412df49f25059ed49d93210067
iwood55 <isaac.ewood@icloud.com>
2021-07-21 14:24:53 -0400

    flex mvp done

---
+++ friends/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Router, Route, Switch, Link } from 'react-router-dom';
+import login from './components/Login.js';
+import PrivateRoute from './components/privateRoute';
+import FriendList from './components/friendsList';
+
+import './App.css';
+
+function App() {
+
+const logout = () => { localStorage.removeItem('token')
+};
+
+return (
+  <Router>
+  <div className="App">
+      <h1>Your Friends</h1>
+    <div className='link1'>
+      <Link to='/login' component={login}>Login here</Link>
+    </div>
+
+    <div>
+      {localStorage.getItem('token') && <Link to="/protected"/>}
+      <Link onClick={logout}>Logout here</Link>
+    </div>
+    <Switch>
+      <PrivateRoute exact path="/protected" component={FriendList} />
+        <Route path="/login" component={login} />
+        <Route component={login} />
+    </Switch>
+  </div>
+  </Router>
+)};
+
+export default App;
